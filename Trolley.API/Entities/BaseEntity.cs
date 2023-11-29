5fabab55dd2a7d7ba2d304b31e691e05bea79501
Stefan Matzner <stefanmatzner@gmx.at>
2023-11-29 13:25:01 +0100

    removed createdAt and modifiedAt from BaseEntity.cs

--- Trolley.API/Entities/BaseEntity.cs
+++ Trolley.API/Entities/BaseEntity.cs
@@ -6,18 +6,6 @@ namespace Trolley.API.Entities
     public class BaseEntity
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Required]
         public int Id { get; set; }
-
-
-        public DateTime DateCreated { get; set; }
-
-        public DateTime DateModified { get; set; }
-
-
-        [ConcurrencyCheck]
-        [Timestamp]
-        public byte[] Timestamp { get; set; }
     }
 }
